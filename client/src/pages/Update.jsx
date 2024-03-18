fe1fee4eca5bf402cf5a80db8d5458b18801dbc3
nrm00014 <nrm00014@mix.wvu.edu>
2024-03-17 23:07:41 -0400

    initial commit

---
+++ client/src/pages/Update.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import axios from "axios";
+
+const Update = () => {
+    const [book,setBook] = useState({
+      title:"",
+      dest:"",
+      price:null,
+      cover:"",
+    });
+
+    const handleChange = e =>{
+      setBook((prev) =>({...prev, [e.target.name]: e.target.value}));
+    };
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const bookId = location.pathname.split("/")[2];
+
+    const handleClick = async e =>{
+        e.preventDefault()
+        try{
+            await axios.put("http://localhost:8800/books/"+bookId, book);
+            navigate("/");
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+
+    console.log(book);
+
+    return( 
+    <div className = "form">
+        <h1>Update the Book</h1>
+            <input type = "text" placeholder = "title" onChange = {handleChange} name = "title" />
+            <input type = "text" placeholder = "desc" onChange = {handleChange} name = "dest" />
+            <input type = "number" placeholder = "price" onChange = {handleChange} name = "price"/>
+            <input type = "text" placeholder = "cover" onChange = {handleChange} name = "cover" />
+
+            <button className = "formButton" onClick = {handleClick}>Update</button>
+    </div>
+    );
+    
+};
+
+export default Update
